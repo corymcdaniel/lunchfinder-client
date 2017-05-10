19f504300c891337e299c53a13c98c13d996840a
Cory <corymcdaniel@gmail.com>
2017-05-10 18:14:19 -0400

    fix link

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,27 +5,17 @@ import * as authActions from '../../actions/authActions';
 class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.facebookLogin = this.facebookLogin.bind(this);
-  }
-
-  facebookLogin() {
-    console.log('here');
-    authActions.facebookLogin();
   }
 
   render() {
     return (
       <div className="jumbotron">
         <h1>Where's Lunch?</h1>
-        <p><a onClick={this.facebookLogin}>Login with Facebook</a></p>
+        <p><a href="http://localhost:3300/v1/auth/facebook">Login with Facebook</a></p>
         <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
     );
   }
 }
-
-HomePage.propTypes = {
-  actions: PropTypes.object.isRequired
-};
 
 export default HomePage;
