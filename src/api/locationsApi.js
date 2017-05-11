133db6bee5625e883aa0a405d7ef305956834aaa
Cory <corymcdaniel@gmail.com>
2017-05-11 16:40:27 -0500

    Better handling of api responses, abstract out api url

--- src/api/locationsApi.js
+++ src/api/locationsApi.js
@@ -1,53 +1,10 @@
-const mockData = [
-    {
-      name: 'Which Wich',
-      lat: '0',
-      long: '0',
-      distance: '0.3km'
-    },
-    {
-      name: 'Mojo\'s',
-      lat: '0',
-      long: '0',
-      distance: '1.3km'
-    },
-    {
-      name: 'Cypress St. P&P',
-      lat: '0',
-      long: '0',
-      distance: '2.5km'
-    }
-  ];
-
+import { HandleResponse } from './responseHandler';
+//ESLint doesn't detect webpack globals:
+/* global API_URL */
 class LocationsApi {
   static getNearbyLocations(address) {
-    //return new Promise((resolve, reject) => {
-      let url = 'http://localhost:3300/v1/locations?address=' + address;
-      return fetch(url).then(response => {
-        return response.text().then((json) => {
-          debugger;
-          try {
-            if(response.ok) {
-              if(json) {
-                let r = JSON.parse(json);
-                if(response.headers.has('X-Total-Count')) {
-                  r = {
-                    total: Number(response.headers.get('X-Total-Count')),
-                    items: r
-                  };
-                }
-                return r;
-              }
-            } else {
-              return {_error: json || `Failed with status code ${response.status}`};
-            }
-          } catch(err) {
-            return {_error: `Failed with status code ${response.status}`};
-          }
-        });
-        //return Object.assign([], response.locations);
-      });
-    //});
+      let url = `${API_URL}v1/locations?address=${address}`;
+      return fetch(url).then(HandleResponse);
   }
 }
 
--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -2,7 +2,6 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function authorReducer(state = initialState.locations, action) {
-  console.log(action);
   switch(action.type) {
     case types.LOAD_LOCATIONS_SUCCESS:
       return action.locations;
