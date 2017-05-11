665b9134d9e95eea52ef9711fd5b74558271f403
Cory <corymcdaniel@gmail.com>
2017-05-10 20:30:22 -0400

    Get coords

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
+import {geolocated} from 'react-geolocated';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
         <h1>Where's Lunch?</h1>
-        <p></p>
+        {this.props.isGeolocationAvailable &&
+          this.props.coords && <p>{this.props.coords.latitude} - {this.props.coords.longitude}</p>}
         <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
     );
   }
 }
 
-export default HomePage;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000
+})(HomePage);
