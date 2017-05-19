36b339f8ca04955f9d4ff57bc17ca6e00cba6d6c
Cory <corymcdaniel@gmail.com>
2017-05-19 13:27:04 -0400

    Show better dates

--- src/components/reviews/reviewListItem.js
+++ src/components/reviews/reviewListItem.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import moment from 'moment';
 import Rating from './rating';
 
 const ReviewListItem = ({user, rating, comment, createdAt}) => {
@@ -6,7 +7,7 @@ const ReviewListItem = ({user, rating, comment, createdAt}) => {
     <li className="list-item media">
       <div className="media-body">
         <h4 className="media-heading">{user.name}</h4>
-        <h5>{createdAt}</h5>
+        <h5>{moment(createdAt).fromNow()}</h5>
         <p><Rating rating={rating} /></p>
         <p>{comment}</p>
       </div>
